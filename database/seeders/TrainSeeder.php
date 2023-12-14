41b1ccae06240ecaba33da06c5ef73ce3fa83862
Anse98 <lorenzo.anselmi98.com>
2023-12-14 14:48:26 +0100

    ADD-Bonus, quindi l'orario di partenza sarà sempre inferiore all'orario di arrivo

--- database/seeders/TrainSeeder.php
+++ database/seeders/TrainSeeder.php
@@ -21,8 +21,11 @@ class TrainSeeder extends Seeder
             $new_train->stazione_arrivo = $faker->city();
             $new_train->giorno_partenza = $faker->dateTimeThisYear('+2 months');
             $new_train->giorno_arrivo = $new_train->giorno_partenza;
-            $new_train->orario_partenza = $faker->time();
-            $new_train->orario_arrivo = $faker->time();
+            $randomTimeStart = time();
+            $randomAddHour = $faker->numberBetween(1200, 10000);
+            $randomTimeEnd = $randomTimeStart + $randomAddHour;
+            $new_train->orario_partenza = date('Y-m-d H:i:s', $randomTimeStart);
+            $new_train->orario_arrivo = date('Y-m-d H:i:s', $randomTimeEnd);
             $new_train->codice_treno = $faker->unique()->randomNumber(9, true);
             $new_train->numero_carrozze = $faker->randomDigitNotNull();
             $new_train->save();
